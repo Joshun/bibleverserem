ff5e209babc648c8e51f593fde83c92325cbc7a3
Joshua O'Leary <jmoey139@gmail.com>
2018-09-29 15:03:05 +0100

    first commit of code

---
+++ config.py
@@ -0,0 +1,12 @@
+
+
+passages = [
+    "Ephesians 5:25-26",
+    "John 3:16"
+]
+
+settings = {
+    # cycle time in mins
+    "cycle_time": 0.1,
+    "split_words": 30
+}
---
+++ main.py
@@ -0,0 +1,60 @@
+import requests
+import config
+from win10toast import ToastNotifier
+from time import sleep
+import math
+from auth import auth_token
+
+
+#r = requests.get(
+#    'https://api.esv.org/v3/passage/text/?q=John+11:35-John+11:37',
+#    params={'include-verse-numbers': 'false'},
+#    headers={'Authorization': 'Token 4f5ab1c47d563c0dff34f2bd152619ddc7dab676'}
+#    )
+
+
+def get_passage_text(passage):
+    encoded_passage = passage.replace(" ", "+")
+    r = requests.get(
+        'https://api.esv.org/v3/passage/text/?q={0}'.format(encoded_passage),
+        params={'include-verse-numbers': 'false', 'include-headings': 'false', 'include-footnotes': 'false'},
+        headers={'Authorization': 'Token 4f5ab1c47d563c0dff34f2bd152619ddc7dab676'}
+        )
+    return r
+
+#   print(r.json()['passages'])
+
+toaster = ToastNotifier()
+
+while True:
+    for passage in config.passages:
+        # code here
+        r = get_passage_text(passage)
+        passage_text = r.json()['passages'][0]
+
+        #toaster.show_toast('bibverserem', r.json()['passages'][0], duration=10)
+        print(passage_text)
+        reference, verses = passage_text.split('\n\n')
+        print(verses)
+            
+
+        passage_text_words = verses.split(" ")
+
+        splits = math.ceil(len(passage_text_words)/int(config.settings["split_words"]))
+
+        for split in range(splits):
+            print("split: " + str(split))
+            split_passage = passage_text_words[split*config.settings["split_words"]:(split+1)*config.settings["split_words"]]
+            print(split_passage)
+            toaster.show_toast(reference, " ".join(split_passage), duration=math.ceil(2 + 0.1*len(passage_text_words)))
+            
+
+
+        sleep(int(config.settings["cycle_time"]) * 60)
+
+
+#passage = r.json()['passages'][0]
+
+#reference, verses = passage.split('\n\n')
+#toaster.show_toast(reference, verses, duration=10)
+
